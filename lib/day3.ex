fc5043babb451d0bf86c3d55c4c2c25201776a29
Danny Burkes <dburkes@netable.com>
2025-12-06 16:58:14 -0800

    day 3 part 1 (really)

--- lib/day3.ex
+++ lib/day3.ex
@@ -1,2 +1,33 @@
 defmodule Day3 do
+  def part1() do
+    parse()
+    |> Enum.map(&find_common_item/1)
+    |> Enum.map(&priority/1)
+    |> Enum.sum()
+  end
+
+  def find_common_item({left, right}) do
+    left_chars = String.codepoints(left)
+    right_chars = String.codepoints(right)
+
+    Enum.find(left_chars, &Enum.member?(right_chars, &1))
+  end
+
+  def priority(item) do
+    if String.upcase(item) == item do
+      hd(String.to_charlist(item)) - ?A + 27
+    else
+      hd(String.to_charlist(item)) - ?a + 1
+    end
+  end
+
+  def parse(input \\ File.read!("lib/fixtures/day3.txt")) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      halfs = String.split_at(line, div(String.length(line), 2))
+      {elem(halfs, 0), elem(halfs, 1)}
+    end)
+  end
 end
--- test/day3_test.exs
+++ test/day3_test.exs
@@ -1,3 +1,58 @@
 defmodule Day3Test do
   use ExUnit.Case
+
+  @input """
+  vJrwpWtwJgWrhcsFMMfFFhFp
+  jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+  PmmdzqPrVvPwwTWBwg
+  wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+  ttgJtRGJQctTZtZT
+  CrZsJsPPZsGzwwsLwLmpwMDw
+  """
+
+  test "parsing" do
+    sacks = Day3.parse(@input)
+
+    assert length(sacks) == 6
+
+    assert String.length(elem(Enum.at(sacks, 0), 0)) == 12
+    assert String.length(elem(Enum.at(sacks, 0), 1)) == 12
+
+    assert String.length(elem(Enum.at(sacks, 1), 0)) == 16
+    assert String.length(elem(Enum.at(sacks, 1), 1)) == 16
+
+    assert String.length(elem(Enum.at(sacks, 2), 0)) == 9
+    assert String.length(elem(Enum.at(sacks, 2), 1)) == 9
+
+    assert String.length(elem(Enum.at(sacks, 3), 0)) == 15
+    assert String.length(elem(Enum.at(sacks, 3), 1)) == 15
+
+    assert String.length(elem(Enum.at(sacks, 4), 0)) == 8
+    assert String.length(elem(Enum.at(sacks, 4), 1)) == 8
+
+    assert String.length(elem(Enum.at(sacks, 5), 0)) == 12
+    assert String.length(elem(Enum.at(sacks, 5), 1)) == 12
+  end
+
+  test "finding common item" do
+    sacks = Day3.parse(@input)
+
+    assert Day3.find_common_item(Enum.at(sacks, 0)) == "p"
+    assert Day3.find_common_item(Enum.at(sacks, 1)) == "L"
+    assert Day3.find_common_item(Enum.at(sacks, 2)) == "P"
+    assert Day3.find_common_item(Enum.at(sacks, 3)) == "v"
+    assert Day3.find_common_item(Enum.at(sacks, 4)) == "t"
+    assert Day3.find_common_item(Enum.at(sacks, 5)) == "s"
+  end
+
+  test "priority" do
+    assert Day3.priority("a") == 1
+    assert Day3.priority("m") == 13
+    assert Day3.priority("A") == 27
+    assert Day3.priority("M") == 39
+  end
+
+  test "part1" do
+    assert Day3.part1() == 0
+  end
 end
