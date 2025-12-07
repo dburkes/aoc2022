646ffd658f5adf1301c662b1f17785b1816d0fd5
Danny Burkes <dburkes@netable.com>
2025-12-07 15:17:00 -0800

    day 6 part 2

--- lib/day6.ex
+++ lib/day6.ex
@@ -1,13 +1,23 @@
 defmodule Day6 do
   def part1() do
-    find_marker(File.read!("lib/fixtures/day6.txt"))
+    find_marker(File.read!("lib/fixtures/day6.txt"), :packet)
   end
 
-  def find_marker(input) do
+  def part2() do
+    find_marker(File.read!("lib/fixtures/day6.txt"), :message)
+  end
+
+  def find_marker(input, type) do
+    offset =
+      case type do
+        :packet -> 4
+        :message -> 14
+      end
+
     input
     |> String.graphemes()
-    |> Enum.chunk_every(4, 1, :discard)
+    |> Enum.chunk_every(offset, 1, :discard)
     |> Enum.find_index(fn chunk -> Enum.uniq(chunk) == chunk end)
-    |> Kernel.+(4)
+    |> Kernel.+(offset)
   end
 end
--- test/day6_test.exs
+++ test/day6_test.exs
@@ -1,14 +1,28 @@
 defmodule Day6Test do
   use ExUnit.Case
 
-  test "marker finding" do
-    assert Day6.find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
-    assert Day6.find_marker("nppdvjthqldpwncqszvftbrmjlhg") == 6
-    assert Day6.find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
-    assert Day6.find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
+  describe "finding" do
+    test "packet markers" do
+      assert Day6.find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", :packet) == 5
+      assert Day6.find_marker("nppdvjthqldpwncqszvftbrmjlhg", :packet) == 6
+      assert Day6.find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", :packet) == 10
+      assert Day6.find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", :packet) == 11
+    end
+
+    test "message markers" do
+      assert Day6.find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", :message) == 19
+      assert Day6.find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", :message) == 23
+      assert Day6.find_marker("nppdvjthqldpwncqszvftbrmjlhg", :message) == 23
+      assert Day6.find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", :message) == 29
+      assert Day6.find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", :message) == 26
+    end
   end
 
   test "part1" do
     assert Day6.part1() == 1480
+  end
+
+  test "part2" do
+    assert Day6.part2() == 2746
   end
 end
