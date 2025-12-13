450730a43e24281d65cbc390f9f79ff641b43ff4
Danny Burkes <dburkes@netable.com>
2025-12-13 09:34:14 -0800

    day 10 part 1

---
+++ lib/day10.ex
@@ -0,0 +1,60 @@
+defmodule Day10 do
+  def part1() do
+    parse()
+    |> signal_strength()
+  end
+
+  def signal_strength(program) do
+    {_, _, history} = run(program)
+
+    [20, 60, 100, 140, 180, 220]
+    |> Enum.sum_by(fn cycle_number ->
+      signal_strength(cycle_number, history)
+    end)
+  end
+
+  def signal_strength(cycle, history), do: register_during(cycle, history) * cycle
+
+  def register_during(cycle, history) do
+    Enum.split_with(history, fn {cycle_num, _} -> cycle_num < cycle end)
+    |> elem(0)
+    |> List.last()
+    |> elem(1)
+  end
+
+  def run(program, cycles \\ 1_000_000) do
+    program
+    |> Enum.reduce_while({0, 1, []}, fn {instruction, param}, {cycle_num, register, history} ->
+      instruction_cycles =
+        case(instruction) do
+          :noop -> 1
+          :addx -> 2
+        end
+
+      cond do
+        cycle_num + instruction_cycles <= cycles ->
+          new_reg = register + param
+          new_cycle = cycle_num + instruction_cycles
+
+          if new_cycle == cycles do
+            {:halt, {cycles, new_reg, [{cycles, new_reg} | history]}}
+          else
+            {:cont, {new_cycle, new_reg, [{new_cycle, new_reg} | history]}}
+          end
+
+        true ->
+          {:halt, {cycles, register, [{cycles, register} | history]}}
+      end
+    end)
+    |> then(fn {cycles, register, history} -> {cycles, register, Enum.reverse(history)} end)
+  end
+
+  def parse(input \\ File.read!("lib/fixtures/day10.txt")) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_instruction/1)
+  end
+
+  defp parse_instruction("noop"), do: {:noop, 0}
+  defp parse_instruction("addx " <> value), do: {:addx, String.to_integer(value)}
+end
---
+++ test/day10_test.exs
@@ -0,0 +1,205 @@
+defmodule Day9Test do
+  use ExUnit.Case
+
+  @input """
+  addx 15
+  addx -11
+  addx 6
+  addx -3
+  addx 5
+  addx -1
+  addx -8
+  addx 13
+  addx 4
+  noop
+  addx -1
+  addx 5
+  addx -1
+  addx 5
+  addx -1
+  addx 5
+  addx -1
+  addx 5
+  addx -1
+  addx -35
+  addx 1
+  addx 24
+  addx -19
+  addx 1
+  addx 16
+  addx -11
+  noop
+  noop
+  addx 21
+  addx -15
+  noop
+  noop
+  addx -3
+  addx 9
+  addx 1
+  addx -3
+  addx 8
+  addx 1
+  addx 5
+  noop
+  noop
+  noop
+  noop
+  noop
+  addx -36
+  noop
+  addx 1
+  addx 7
+  noop
+  noop
+  noop
+  addx 2
+  addx 6
+  noop
+  noop
+  noop
+  noop
+  noop
+  addx 1
+  noop
+  noop
+  addx 7
+  addx 1
+  noop
+  addx -13
+  addx 13
+  addx 7
+  noop
+  addx 1
+  addx -33
+  noop
+  noop
+  noop
+  addx 2
+  noop
+  noop
+  noop
+  addx 8
+  noop
+  addx -1
+  addx 2
+  addx 1
+  noop
+  addx 17
+  addx -9
+  addx 1
+  addx 1
+  addx -3
+  addx 11
+  noop
+  noop
+  addx 1
+  noop
+  addx 1
+  noop
+  noop
+  addx -13
+  addx -19
+  addx 1
+  addx 3
+  addx 26
+  addx -30
+  addx 12
+  addx -1
+  addx 3
+  addx 1
+  noop
+  noop
+  noop
+  addx -9
+  addx 18
+  addx 1
+  addx 2
+  noop
+  noop
+  addx 9
+  noop
+  noop
+  noop
+  addx -1
+  addx 2
+  addx -37
+  addx 1
+  addx 3
+  noop
+  addx 15
+  addx -21
+  addx 22
+  addx -6
+  addx 1
+  noop
+  addx 2
+  addx 1
+  noop
+  addx -10
+  noop
+  noop
+  addx 20
+  addx 1
+  addx 2
+  addx 2
+  addx -6
+  addx -11
+  noop
+  noop
+  noop
+  """
+
+  test "parsing" do
+    data = Day10.parse(@input)
+    assert length(data) == 146
+    assert Enum.at(data, 0) == {:addx, 15}
+  end
+
+  describe "running" do
+    test "a partial program" do
+      data = Day10.parse(@input)
+      assert {4, 5, [{2, 16}, {4, 5}]} = Day10.run(data, 4)
+    end
+
+    test "a full program" do
+      data = Day10.parse(@input)
+      assert {240, 17, _} = Day10.run(data)
+    end
+  end
+
+  test "register probing" do
+    data = Day10.parse(@input)
+    {_, _, history} = Day10.run(data)
+
+    assert Day10.register_during(20, history) == 21
+    assert Day10.register_during(60, history) == 19
+    assert Day10.register_during(100, history) == 18
+    assert Day10.register_during(140, history) == 21
+    assert Day10.register_during(180, history) == 16
+    assert Day10.register_during(220, history) == 18
+  end
+
+  describe "signal strength" do
+    test "at cycle" do
+      data = Day10.parse(@input)
+      {_, _, history} = Day10.run(data)
+
+      assert Day10.signal_strength(20, history) == 420
+      assert Day10.signal_strength(60, history) == 1140
+      assert Day10.signal_strength(100, history) == 1800
+      assert Day10.signal_strength(140, history) == 2940
+      assert Day10.signal_strength(180, history) == 2880
+      assert Day10.signal_strength(220, history) == 3960
+    end
+
+    test "of program" do
+      data = Day10.parse(@input)
+      assert Day10.signal_strength(data) == 13140
+    end
+  end
+
+  test "part 1" do
+    assert Day10.part1() == 14920
+  end
+end
